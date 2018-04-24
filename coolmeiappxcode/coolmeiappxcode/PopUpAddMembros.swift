c8575de13cb9f6ce88a992bc926e3649d73dac74
Unknown <Flavia.n.godoy@gmail.com>
2018-04-24 16:06:01 -0300

    testando aparecer a xib popupaddmembros

---
+++ coolmeiappxcode/coolmeiappxcode/PopUpAddMembros.swift
@@ -0,0 +1,32 @@
+//
+//  PopUpAddMembros.swift
+//  coolmeiappxcode
+//
+//  Created by Aluno on 24/04/18.
+//  Copyright © 2018 Fem.me. All rights reserved.
+//
+
+import UIKit
+
+class PopUpAddMembros: UIView {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var labelView: UILabel!
+    
+    override init(frame: CGRect) {  // usando a customview no codigo
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) { // usando na IB
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed("PopUpAddMembros", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+}
