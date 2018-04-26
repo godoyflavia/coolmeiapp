73f9983bcd46c28b41585c5ca233c6c54156d343
hallow65 <camilasimoes65@gmail.com>
2018-04-26 19:39:13 -0300

    teste do desalinhamento das células e cocoa pod
    sei la se funcionou essa merda

---
+++ teste CocoaPod/Pods/Target Support Files/Pods-teste CocoaPod/Pods-teste CocoaPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_teste_CocoaPod : NSObject
+@end
+@implementation PodsDummy_Pods_teste_CocoaPod
+@end
---
+++ teste CocoaPod/teste CocoaPod/TesteCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TesteCollectionViewCell.swift
+//  teste CocoaPod
+//
+//  Created by Aluno on 26/04/2018.
+//  Copyright © 2018 fem.me. All rights reserved.
+//
+
+import UIKit
+
+class TesteCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var labelTeste: UILabel!
+}
