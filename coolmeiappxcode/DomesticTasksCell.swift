3b3ca70863e9bda113476b04498d06d06a1c6c0b
hallow65 <camilasimoes65@gmail.com>
2018-04-28 17:56:56 -0300

    collection view da tela inicial (do jeito normal)
    delegate e dataSource dela como extension
    >> o popUp ta escondido atras da collection, nao se desesperem <<

---
+++ coolmeiappxcode/DomesticTasksCell.swift
@@ -0,0 +1,17 @@
+//
+//  DomesticTasksCell.swift
+//  coolmeiappxcode
+//
+//  Created by Camila Simões on 28/04/18.
+//  Copyright © 2018 Fem.me. All rights reserved.
+//
+
+import UIKit
+
+class DomesticTasksCell: UICollectionViewCell {
+ 
+  @IBOutlet weak var taskIcon: UIImageView!
+  
+  @IBOutlet weak var memberColor: UIImageView!
+  
+}
--- coolmeiappxcode/coolmeiappxcode/ViewController.swift
+++ coolmeiappxcode/coolmeiappxcode/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var domesticTasksCollection: UICollectionView!
+  
+  
     @IBOutlet weak var PopUpAddMembros: PopUpAddMembros!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      
+      self.domesticTasksCollection.dataSource = self
+      self.domesticTasksCollection.delegate = self
+      
+      
+      
         PopUpAddMembros.labelView.text = "milena é poc"
+      // se vc veio aqui tentando descobrir pq o pop-up nao aparece
+      // eh pq ele tem q ficar em cima de tudo no storyboard == ser o último dos arquivinhos
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +33,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+//MARK: DataSource da CollectionVIew
+extension ViewController: UICollectionViewDataSource {
+
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 12
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    let cell: DomesticTasksCell = collectionView.dequeueReusableCell(withReuseIdentifier: "taskCell", for: indexPath) as! DomesticTasksCell
+    
+    cell.taskIcon.image = UIImage(named: "hexagon.png")
+    
+    return cell
+  }
+  
+}
+
+
+//MARK: Delegate da CollectionView
+extension ViewController: UICollectionViewDelegate {
+  
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    
+    let cell: DomesticTasksCell = collectionView.cellForItem(at: indexPath)
+      as! DomesticTasksCell
+    
+    if cell.taskIcon.image == UIImage(named: "hexagon.png") {
+      cell.taskIcon.image = UIImage(named: "hexagon-black.png")
+    } else {
+      cell.taskIcon.image = UIImage(named: "hexagon.png")
+    }
+    
+    //domesticTasksCollection.reloadData()
+    
+  }
+  
+  
+}
